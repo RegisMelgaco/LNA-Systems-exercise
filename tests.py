88484d8cfe868831a95ec8dc02f7cf59db369f41
RegisMelgaco <regis.melgaco@gmail.com>
2019-09-05 10:46:10 -0300

    change tests to pytest

--- tests.py
+++ tests.py
@@ -1,80 +1,67 @@
-import unittest
-from solution import calc_call_cost, calc_total_cost, get_charged_calls, Call
+from solution import Call, calc_call_cost, get_charged_calls, calc_total_cost
 
 
-class SolutionTestCase(unittest.TestCase):
-
-    def setUp(self):
-        self.calls = [
-            Call(0, "1", "2"),
-            Call(1, "1", "2"),
-            Call(1, "2", "1"),
-            Call(59, "1", "2"),
-            Call(60, "1", "2"),
-            Call(61, "1", "2"),
-            Call(5*60, "1", "2"),
-            Call(6*60 - 1, "1", "2"),
-            Call(6*60, "3", "1"),
-            Call(6*60 + 1, "1", "2"),
-        ]
-
-    def test_calc_call_cost_less_and_equals_to_5min(self):
-        self.assertEqual(
-            calc_call_cost(self.calls[0]),
-            0
-        )
-        self.assertEqual(
-            calc_call_cost(self.calls[3]),
-            0.05
-        )
-        self.assertEqual(
-            calc_call_cost(self.calls[4]),
-            0.05
-        )
-        self.assertEqual(
-            calc_call_cost(self.calls[5]),
-            0.10
-        )
-        self.assertEqual(
-            calc_call_cost(self.calls[6]),
-            0.25
-        )
-
-    def test_calc_call_cost_more_than_5min(self):
-        self.assertEqual(
-            calc_call_cost(self.calls[7]),
-            0.27
-        )
-        self.assertEqual(
-            calc_call_cost(self.calls[8]),
-            0.27
-        )
-        self.assertEqual(
-            calc_call_cost(self.calls[9]),
-            0.29
-        )
-
-    def test_get_charged_calls(self):
-        self.assertEqual(len(list(get_charged_calls(self.calls[0:1]))), 0)
-
-        self.assertEqual(len(list(get_charged_calls(self.calls[0:2]))), 0)
-
-        self.assertEqual(
-            list(get_charged_calls(self.calls[1:3]))[0].caller,
-            "2"
-            )
-
-        self.assertEqual(
-            list(get_charged_calls([self.calls[2], self.calls[1]]))[0].caller,
-            "1"
-            )
-
-    def test_calc_total_cost(self):
-        self.assertEqual(calc_total_cost([]), 0)
-        self.assertEqual(calc_total_cost(self.calls[0:1]), 0)
-
-        self.assertEqual(calc_total_cost(self.calls), 0.32)
+calls = [
+    Call(0, "1", "2"),
+    Call(1, "1", "2"),
+    Call(1, "2", "1"),
+    Call(59, "1", "2"),
+    Call(60, "1", "2"),
+    Call(61, "1", "2"),
+    Call(5*60, "1", "2"),
+    Call(6*60 - 1, "1", "2"),
+    Call(6*60, "3", "1"),
+    Call(6*60 + 1, "1", "2"),
+]
 
 
-if __name__ == '__main__':
-    unittest.main()
+class TestCalcCallCost:
+
+    def test_0sec_duration(self):
+        assert calc_call_cost(calls[0]) == 0
+
+    def test_59sec_duration(self):
+        assert calc_call_cost(calls[3]) == 0.05
+
+    def test_60sec_duration(self):
+        assert calc_call_cost(calls[4]) == 0.05
+
+    def test_61sec_duration(self):
+        assert calc_call_cost(calls[5]) == 0.10
+
+    def test_5min_duration(self):
+        assert calc_call_cost(calls[6]) == 0.25
+
+    def test_5min_and_59sec_duration(self):
+        assert calc_call_cost(calls[7]) == 0.27
+
+    def test_6min_duration(self):
+        assert calc_call_cost(calls[8]) == 0.27
+
+    def test_6min_and_1sec_duration(self):
+        assert calc_call_cost(calls[9]) == 0.29
+
+
+class TestGetChargedCalls:
+
+    def test_no_calls(self):
+        assert list(get_charged_calls([])) == []
+
+    def test_only_not_charged_calls(self):
+        assert list(get_charged_calls(calls[0:2])) == []
+
+    def test_dont_charge_first_biggest_caller(self):
+        assert list(get_charged_calls(calls[1:3])) == [calls[2]]
+        assert list(get_charged_calls([calls[2], calls[1]])) == [calls[1]]
+
+
+class TestCalcTotalCost:
+
+    def test_no_calls(self):
+        assert calc_total_cost([]) == 0
+
+    def test_with_no_charged_calls(self):
+        assert calc_total_cost(calls[0:2]) == 0
+
+    def test_multiple_callers(self):
+        assert calc_total_cost(calls) == 0.32
